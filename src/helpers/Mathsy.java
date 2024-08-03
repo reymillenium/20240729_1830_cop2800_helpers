f66b161ac48f6a75c2146fb2c49fd34781991894
Reinier Garcia Ramos <reymillenium@gmail.com>
2024-08-02 21:14:43 -0400

    Updated the Mathsy copyright

--- src/helpers/Mathsy.java
+++ src/helpers/Mathsy.java
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2024 | Reinier Garcia Ramos | reymillenium@gmail.com | https://www.reiniergarcia.dev/
  *
- * Mathsy (Version 2024.08.02.0154)
+ * Mathsy (Version 2024.08.02.2114)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
