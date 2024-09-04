a93c5eaf32fad2b57d64b15abf989335cac47a15
Reinier Garcia Ramos <reymillenium@gmail.com>
2024-09-04 09:12:34 -0400

    Fixed a small typo on a comment

--- src/dev/reiniergarcia/helpers/DatabaseHelper.java
+++ src/dev/reiniergarcia/helpers/DatabaseHelper.java
@@ -29,7 +29,7 @@ package dev.reiniergarcia.helpers;
 import java.util.Random;
 
 public class DatabaseHelper {
-    // Generates a Universally Unique IDentifier (the usual 36-character alphanumeric string. UUID style) as a string. Format: bdc0a2fb-d39e-0242-9a0a-4e760153f18d
+    // Generates a Universally Unique Identifier (the usual 36-character alphanumeric string. UUID style) as a string. Format: bdc0a2fb-d39e-0242-9a0a-4e760153f18d
     public static String getUUID() {
         int min = 0; // Minimum value of the range (inclusive)
         int max = 15; // Maximum value of the range (inclusive)
